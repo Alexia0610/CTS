9eb562d699d7fd86a9899517e1dc47714792da46
Alexia0610 <75184858+Alexia0610@users.noreply.github.com>
2022-05-27 00:43:56 +0300

    lab09

---
+++ gr1085/lab09_flyweightdp/Main.java
@@ -0,0 +1,11 @@
+package gr1085.lab09_flyweightdp;
+
+public class Main {
+
+    public static void main(String[] args) {
+       MasinaFactory.getMasina(TipMasinaDeAfisat.AUTOTURISM_GALBEN).afiseaza();
+       MasinaFactory.getMasina(TipMasinaDeAfisat.AUTOTURISM_GALBEN).afiseaza();
+
+
+    }
+}
---
+++ gr1085/lab09_flyweightdp/TipMasinaDeAfisat.java
@@ -0,0 +1,8 @@
+package gr1085.lab09_flyweightdp;
+
+public enum TipMasinaDeAfisat {
+    AUTOTURISM_VERDE,
+    AUTOTURISM_GALBEN,
+    CAMION_VERDE,
+    CAMION_GALBEN
+}
