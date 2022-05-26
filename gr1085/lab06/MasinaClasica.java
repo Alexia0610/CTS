b796e0dff34dddbc4e6356f43079e9e195c74128
Alexia0610 <75184858+Alexia0610@users.noreply.github.com>
2022-05-27 00:21:54 +0300

    lab06

---
+++ gr1085/lab06/MasinaClasica.java
@@ -0,0 +1,26 @@
+package gr1085.lab06;
+
+public class MasinaClasica extends Masina{
+
+    private String aditivCompatibil;
+
+    public MasinaClasica() {
+    }
+
+    public MasinaClasica(String producator, String sasiu, String culoare, String aditivCompatibil) {
+        super(producator, sasiu, culoare);
+        this.aditivCompatibil = aditivCompatibil;
+    }
+
+    @Override
+    public String getDetails() {
+        return this.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "MasinaClasica{" + super.toString()+
+                "aditivCompatibil='" + aditivCompatibil + '\'' +
+                "} " ;
+    }
+}
