7a6687886ad85c9429925d280f5f5e91618f65c4
Alexia0610 <75184858+Alexia0610@users.noreply.github.com>
2022-05-27 00:17:58 +0300

    lab05

---
+++ gr1085/lab05/Builder.java
@@ -0,0 +1,10 @@
+package gr1085.lab05;
+
+public class Builder {
+    public Builder addSasiu();
+    public Builder addProducator();
+    public Builder addRoti();
+    public Builder addMotor();
+    public Builder addInterior();
+    public Automobil getAutomobil();
+}
